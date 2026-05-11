0c5f1ecd71b59e71769fc5009f6da94b868442c2
Wevulpis <wevulpis@gmail.com>
2026-05-11 16:31:12 -0300

    Added pandas dynamic scoring

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,46 @@
 from fastapi import FastAPI
 import requests
+import pandas as pd
 
 app = FastAPI()
 
+# Dataset inicial do Pyxis
+dados = {
+    "cidade": [
+        "Ribeirão Preto",
+        "São Paulo",
+        "Campinas",
+        "Sertãozinho"
+    ],
+
+    "score": [
+        82,
+        95,
+        88,
+        75
+    ]
+}
+
+df = pd.DataFrame(dados)
+
+# Rota principal
 @app.get("/")
 def home():
     return {"message": "Pyxis API Online"}
 
+# Consulta de CEP
 @app.get("/cep/{cep}")
 def buscar_cep(cep: str):
     url = f"https://viacep.com.br/ws/{cep}/json/"
     response = requests.get(url)
     return response.json()
 
+# Healthcheck
 @app.get("/health")
 def health():
     return {"status": "online"}
 
+# Consulta de cidade
 @app.get("/cidade/{nome}")
 def buscar_cidade(nome: str):
     return {
@@ -24,13 +48,20 @@ def buscar_cidade(nome: str):
         "status": "Mapeamento iniciado"
     }
 
+# Score inteligente usando pandas
 @app.get("/score/{cidade}")
 def score(cidade: str):
 
-    score = 82
+    resultado = df[
+        df["cidade"].str.lower() == cidade.lower()
+    ]
+
+    if resultado.empty:
+        return {"erro": "Cidade não encontrada"}
+
+    score = int(resultado.iloc[0]["score"])
 
     return {
         "cidade": cidade,
-        "score": score,
-        "categoria": "alto potencial"
+        "score": score
     }
